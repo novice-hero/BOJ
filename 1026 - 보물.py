283d71d40ded0a5c84b883e3955b891063078874
KimYoungWoong <highero10@kakao.com>
2022-01-15 23:01:05 +0900

    그리디 - 실버 4

--- 1026 - 보물.py
+++ 1026 - 보물.py
@@ -1,11 +1,11 @@
-n = int(input())
+N = int(input())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 a.sort()
 b.sort(reverse=True)
 
 answer = []
-for i in range(n):
+for i in range(N):
     answer.append(a[i]*b[i])
 
 print(sum(answer))
