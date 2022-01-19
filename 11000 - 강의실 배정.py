7089f79948081e39b3474973f84c281eb8a75344
KimYoungWoong <highero10@kakao.com>
2022-01-19 12:28:33 +0900

    22.01.18-그리디-골드4

---
+++ 11000 - 강의실 배정.py
@@ -0,0 +1,19 @@
+import heapq
+# PyPy3로 해야 시간 통과 가능
+n = int(input())
+time = []
+for _ in range(n):
+    s, e = map(int, input().split())
+    time.append([s, e])
+time.sort(key=lambda x: x[0])
+
+room = []
+heapq.heappush(room, time[0][1])
+for i in range(1, n):
+    if time[i][0] < room[0]:
+        heapq.heappush(room, time[i][1])
+    else:
+        heapq.heappop(room)
+        heapq.heappush(room, time[i][1])
+
+print(len(room))
