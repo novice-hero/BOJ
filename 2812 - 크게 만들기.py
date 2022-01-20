ccefbfeec1de0053b656eaf34d6a71338e835b1d
KimYoungWoong <highero10@kakao.com>
2022-01-20 15:04:41 +0900

    22.01.19-그리디-골드4

---
+++ 2812 - 크게 만들기.py
@@ -0,0 +1,19 @@
+n, k = map(int, input().split())
+s = input()
+
+stack = []
+
+for i in s:
+    while stack and i > stack[-1]:
+        if k > 0:
+            stack.pop()
+            k -= 1
+        else:
+            break
+    stack.append(i)
+
+if k > 0:
+    for i in range(k):
+        stack.pop()
+
+print(''.join(stack))
