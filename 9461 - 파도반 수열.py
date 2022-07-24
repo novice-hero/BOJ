82fc148d942b32084a6cf78866499e5dc22b7f0a
KimYoungWoong <highero10@kakao.com>
2022-07-24 15:49:18 +0900

    22-07-24 실버 3

---
+++ 9461 - 파도반 수열.py
@@ -0,0 +1,11 @@
+# dp
+T = int(input())
+for _ in range(T):
+  n = int(input())
+  dp = [0]*101
+  dp[1] = 1
+  dp[2] = 1
+  dp[3] = 1
+  for i in range(4,101):
+    dp[i] = dp[i-3]+dp[i-2]
+  print(dp[n])
